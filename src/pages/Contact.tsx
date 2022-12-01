2323d1d7eb1b8764874ce5eada32c907811461e5
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-12-01 10:23:18 +0100

    import react everywhere

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 const Contact: React.FC = (): JSX.Element => {
     return <div>Contact</div>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, process.cwd(), "")
 
     return {
         define: {
             __APP_ENV__: env.APP_ENV,
         },
-        plugins: [],
+        plugins: [react()],
         server: {
             host: true,
             port: 3001,
